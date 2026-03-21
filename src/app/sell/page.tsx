9ffaaf3c310b4a4a3d33e2f19a1bb27e05300754
Nald Aguilar <101144668+aguilarxnldoz@users.noreply.github.com>
2026-03-21 16:29:31 -0700

    Adjust layout of seller page for better alignment

--- src/app/sell/page.tsx
+++ src/app/sell/page.tsx
@@ -9,7 +9,7 @@ export default function SellPage() {
 					<h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none">Selling your home?</h1>
 					<p className="text-muted-foreground mt-4 md:text-xl">Find out what your home is worth. Enter your details below for a free evaluation.</p>
 				</div>
-				<div className="flex w-full flex-col-reverse justify-center">
+				<div className="flex w-full flex-col justify-center">
 					<SellerResources />
 					<div className="bg-card text-card-foreground mx-auto w-full max-w-lg rounded-xl border p-6 shadow-sm sm:p-8">
 						<LeadCaptureForm type="valuation" />
