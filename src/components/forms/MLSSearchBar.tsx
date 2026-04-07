584dcc914f2ccf6f82869500a5ff714f3ca8f3c1
hateNaldoz <aguilarnald@gmail.com>
2026-04-07 13:46:15 -0700

    chore: improve uiux

--- src/components/forms/MLSSearchBar.tsx
+++ src/components/forms/MLSSearchBar.tsx
@@ -7,7 +7,7 @@ export default function MLSSearchBar() {
 				type="text"
 				name="location"
 				placeholder="City, Neighborhood, Address or MLS® Number"
-				className="mx-auto my-1.75 block h-10 w-full rounded-md border-neutral-800 bg-[linear-gradient(142.99deg,rgba(217,217,217,0.63)_15.53%,rgba(243,243,243,0.63)_88.19%)] px-3.75 py-4.5 text-center text-lg text-[rgb(34,34,34)] shadow-md transition-all duration-500 outline-none focus-visible:ring-0 sm:h-12 lg:text-xl"
+				className="mx-auto my-1.75 block h-10 w-full rounded-md border-neutral-800 bg-[linear-gradient(142.99deg,rgba(217,217,217,0.63)_15.53%,rgba(243,243,243,0.63)_88.19%)] px-3.75 py-4.5 text-center text-lg text-[rgb(34,34,34)] shadow-md transition-all duration-500 outline-none focus-visible:ring-0 sm:h-12 lg:text-xl 2xl:bg-neutral-400/70"
 			/>
 		</div>
 	);
